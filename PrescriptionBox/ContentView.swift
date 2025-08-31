e9f3177de6f056aa4747643770645936722c15c3
Adi2K <adityaskadam1@gmail.com>
2025-08-31 23:48:36 +0530

    Initial Commit

---
+++ PrescriptionBox/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PrescriptionBox
+//
+//  Created by Aditya Kadam on 31/08/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PrescriptionBox/PrescriptionBoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrescriptionBoxApp.swift
+//  PrescriptionBox
+//
+//  Created by Aditya Kadam on 31/08/25.
+//
+
+import SwiftUI
+
+@main
+struct PrescriptionBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
